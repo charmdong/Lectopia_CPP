4588c90512e13b924dca8d48f29bfdf4cab8034d
Donggun Chung <chungdk1117@gmail.com>
2017-08-19 16:49:16 +0900

    Time(double) constructor of Time.cpp is changed

--- HW17/Time.cpp
+++ HW17/Time.cpp
@@ -10,7 +10,18 @@ Time::Time(int h, int m): hour(h) , min(m), time(h+(m/60.0))
 
 Time::Time(double t):hour(t),time(t)
 { 
-	this->min = (t - this->hour)*60;
+	// this->min = (time-hour)*60; 
+	/*
+		처음 코딩할 때 위와 같이 구현하였으나,
+		(time-hour)의 값이 0.3 혹은 0.9가 나오는 경우
+		각각 0.2999999~ 와 0.8999999~ 로 인식되어
+		17분, 53분으로 계산되었습니다. 
+		이에 따라서 아래의 코드로 변경하였습니다.
+		(python 과 gcc의 경우도 결과가 같음)
+	*/
+
+	int total = time * 60;
+	this->min = total % 60;
 }
 
 Time::Time(const Time &tr) : hour(tr.hour), min(tr.min), time(tr.time)
